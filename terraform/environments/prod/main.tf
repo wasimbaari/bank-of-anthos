47bdd3b0ddbb4fa2904a69dc9dde81ded9489abf
wasimbaari <mdwasimbaari@gmail.com>
2026-02-23 17:19:35 +0530

    feat: force trigger alb controller deployment

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,3 +1,27 @@
+provider "kubernetes" {
+  host                   = module.compute.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.compute.eks_cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.compute.eks_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.compute.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.compute.eks_cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.compute.eks_cluster_name]
+    }
+  }
+}
+
 module "network" {
   source              = "../../modules/network"
   environment         = var.environment
